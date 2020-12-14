04de83eb8304d3d2bd2301511633c182ad7fe5ca
aderose <sandro.derose28@yahoo.com>
2020-12-14 19:36:33 +0000

    Add a click handler to card component

--- src/Card.js
+++ src/Card.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './styles/Card.css';
 
-function Card({ code, name }) {
+function Card({ code, name, handleClick }) {
   return (
-    <div className="card">
+    <div className="card" onClick={handleClick}>
       <div className="img-container">
         <img
           src={`http://www.geognos.com/api/en/countries/flag/${code}.png`}
