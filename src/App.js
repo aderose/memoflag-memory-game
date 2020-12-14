ec8a7d55d9b658d1d40876a5f7422ae83230ef1e
aderose <sandro.derose28@yahoo.com>
2020-12-14 19:38:51 +0000

    Add game state management

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
-import CardBoard from './CardBoard';
-import './styles/App.css';
 import Instructions from './Instructions';
+import CardBoard from './CardBoard';
+import Utils from './Utils';
+
+import countries from './countries';
+import './styles/App.css';
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
+  const [selectedCards, setSelectedCards] = useState([]);
+
+  const generateCards = (num) =>
+    Utils.generateRandomNumbers([], countries.length, num).map(
+      (index) => countries[index],
+    );
+
+  const [cards, setCards] = useState(generateCards(12));
+
+  const handleClick = (index) => () => {
+    const selectedCard = cards[index];
+
+    if (selectedCards.indexOf(selectedCard) < 0) {
+      setSelectedCards([...selectedCards, selectedCard]);
+      setCards(Utils.shuffle(cards));
+      setScore(() => score + 1);
+    } else {
+      setTopScore(() => (topScore < score ? score : topScore));
+      setScore(0);
+      setCards(generateCards(12));
+      setSelectedCards([]);
+    }
+  };
+
   return (
     <div>
-      <Header title="MemoFlag" score="0" topScore="0" />
+      <Header title="MemoFlag" score={score} topScore={topScore} />
       <Instructions />
-      <CardBoard noOfCards={12} />
+      <CardBoard cards={cards} handleClick={handleClick} />
     </div>
   );
 }
