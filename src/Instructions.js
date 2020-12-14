b90e321e41ef694ddc485be294a0046124204cbc
aderose <sandro.derose28@yahoo.com>
2020-12-14 19:01:45 +0000

    Add instructions component

---
+++ src/Instructions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/Instructions.css';
+
+function Instructions(props) {
+  return (
+    <div className="instructions">
+      <p className="container">
+        Get points by clicking on flags. Don't click on the same flag twice!
+      </p>
+    </div>
+  );
+}
+
+export default Instructions;
