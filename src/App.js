5910098709cca45f4f70b86de0fb98ee33442160
aderose <sandro.derose28@yahoo.com>
2020-12-14 19:02:09 +0000

    Include the Instruction component into the app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import CardBoard from './CardBoard';
 import './styles/App.css';
+import Instructions from './Instructions';
 
 function App() {
   return (
     <div>
       <Header title="MemoFlag" score="0" topScore="0" />
+      <Instructions />
       <CardBoard noOfCards={12} />
     </div>
   );
